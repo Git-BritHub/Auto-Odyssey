b80755a47a281fde084d27e9f71252f67830869a
Brittany Brimley <brittanysapp@gmail.com>
2024-01-25 22:42:40 -0700

    created logic for transmission icon/string in car card

--- types/index.ts
+++ types/index.ts
@@ -14,15 +14,15 @@ export interface SearchManufacturerProps {
 
 export interface CarProps {
   city_mpg: number;
-  class: "string";
+  class: string;
   combination_mpg: number;
   cylinders: number;
   displacement: number;
-  drive: "string";
-  fuel_type: "string";
+  drive: string;
+  fuel_type: string;
   highway_mpg: number;
-  make: "string";
-  model: "string";
-  transmission: "string";
+  make: string;
+  model: string;
+  transmission: string;
   year: number;
 }
