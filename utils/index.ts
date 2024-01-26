39a2273e9db5511b97367af39c0313648b34fe3b
Brittany Brimley <brittanysapp@gmail.com>
2024-01-25 22:04:11 -0700

    created CarCardProps, price calculating tool in utils and defined api types

--- utils/index.ts
+++ utils/index.ts
@@ -9,3 +9,19 @@ export async function fetchCars() {
     const result = await response.json();
     return result;
 }
+
+export const calculateCarRent = (city_mpg: number, year: number) => {
+    // Base rental price per day, additional rate per mile, additional rate per year of car
+    const basePricePerDay = 50;
+    const mileageFactor = 0.1;
+    const ageFactor = 0.05;
+
+    // Calculate additional rate based on mileage and age
+    const mileageRate = city_mpg * mileageFactor;
+    const ageRate = (new Date().getFullYear() - year) * ageFactor;
+
+    // Calculate total rental rate per day
+    const rentalRatePerDay = basePricePerDay + mileageRate + ageRate;
+
+    return rentalRatePerDay.toFixed(0);
+}
