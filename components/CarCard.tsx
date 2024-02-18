be12b7a7b0c6999514ea235b5bd68e32deade8e0
Brittany Brimley <brittanysapp@gmail.com>
2024-02-17 23:33:13 -0700

    commented out custom button import that is not yet finished. will uncomment once button is ready

--- components/CarCard.tsx
+++ components/CarCard.tsx
@@ -2,7 +2,7 @@
 import { useState } from 'react';
 import Image from 'next/image';
 import { CarProps } from '@/types';
-import { CustomButton } from './CustomButton';
+// import { CustomButton } from './CustomButton';
 import { calculateCarRent } from '@/utils';
 
 interface CarCardProps {
